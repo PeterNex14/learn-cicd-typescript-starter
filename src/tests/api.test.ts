2c6aec7ebfe17449296bd8781b5c6337dd305d49
PeterNex14 <peternex42@gmail.com>
2026-02-07 04:33:23 +0800

    add: unit test workflow

---
+++ src/tests/api.test.ts
@@ -0,0 +1,17 @@
+import { expect, describe, test } from "vitest";
+
+const person = {
+    isActive: true,
+    age: 32,
+}
+
+describe("person", () => {
+    test("person is defined", () => {
+        expect(person).toBeUndefined();
+    })
+
+    test("is active", () => {
+        expect(person.isActive).toBeTruthy();
+    })
+})
+
